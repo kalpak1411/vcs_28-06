4aa770020119788af6142dc1c8cdb144f68164fd
kalpak1411 <137478686+kalpak1411@users.noreply.github.com>
2026-03-12 10:18:06 +0530

    Add files via upload

---
+++ construction.js
@@ -0,0 +1,8 @@
+let radius = 20
+
+let area = 3.14 * radius * radius
+console.log("Area of the circle :" + area)
+let peri = 2 * 3.14 * radius
+console.log("Perimeter of the circle :" + peri)
+let diameter = 2 * radius
+console.log(diameter)
